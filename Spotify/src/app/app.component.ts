363b3b2a526d19ce82c8bea25c137dace83dd616
thomaslopezro <thomaslopez0104@hotmail.com>
2024-02-07 21:58:13 -0500

    Avance Thoams

--- Spotify/src/app/app.component.ts
+++ Spotify/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { BarralateralComponent } from './components/barralateral/barralateral.component';
+import { NavComponent } from './components/nav/nav.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, BarralateralComponent],
+  imports: [CommonModule, RouterOutlet, BarralateralComponent, NavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
