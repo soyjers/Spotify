c09ed3f4f77f3915bba2efb2f236dd4a5fd8e362
soyjers <borkgoncoc@gmail.com>
2024-02-13 20:56:56 -0500

    Subida para Juntar el Back

--- Spotify/src/app/components/reproductor/reproductor.component.ts
+++ Spotify/src/app/components/reproductor/reproductor.component.ts
@@ -9,4 +9,20 @@ import { Component } from '@angular/core';
 })
 export class ReproductorComponent {
 
+  iconPlay: string = "assets/img/reproductor/play.svg"
+  imgCancion :string = "assets/img/TQMQA - Eladio Carrion.jpg"
+  artista! : string
+  nombreCancion!:string
+  dataSongs: any = [
+    { src: "assets/songs/eternity-extended-mix.mp3", img: "assets/img/songs/anyma.jpg", nombre: "Eternity", artista: "Anyma" }
+  ]
+
+  audioCancion() {
+    let audio = new Audio(this.dataSongs[0].src)
+    audio.play()
+    this.iconPlay = "assets/img/reproductor/repeat.svg"
+    this.imgCancion = this.dataSongs[0].img
+    this.artista = this.dataSongs[0].artista
+    this.nombreCancion = this.dataSongs[0].nombre
+  }
 }
