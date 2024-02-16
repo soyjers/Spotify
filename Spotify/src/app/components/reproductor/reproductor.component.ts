69c2c0a82fcf4050e951d771b3b5ac1361dd3de6
soyjers <borkgoncoc@gmail.com>
2024-02-15 19:01:37 -0500

    Se Arreglo la pagina Principal, Pagina Favoritos, Se arreglo la barra de Reproduccion y se finiquitaron detalles en otros componentes

--- Spotify/src/app/components/reproductor/reproductor.component.ts
+++ Spotify/src/app/components/reproductor/reproductor.component.ts
@@ -15,9 +15,9 @@ import { Router, RouterLink} from '@angular/router';
 export class ReproductorComponent {
 
   iconPlay: string = "assets/img/reproductor/play.svg"
-  imgCancion :string = "assets/img/TQMQA - Eladio Carrion.jpg"
-  artista! : string
-  nombreCancion!:string
+  imgCancion :string = "assets/img/reproductor/IconDisc.png"
+  artista : string = "Sin Definir"
+  nombreCancion:string = "Esperando para Reproducir"
   dataSongs: any = [
     { src: "assets/songs/eternity-extended-mix.mp3", img: "assets/img/songs/anyma.jpg", nombre: "Eternity", artista: "Anyma" }
   ]
@@ -25,7 +25,7 @@ export class ReproductorComponent {
   audioCancion() {
     let audio = new Audio(this.dataSongs[0].src)
     audio.play()
-    this.iconPlay = "assets/img/reproductor/repeat.svg"
+    this.iconPlay = "assets/img/reproductor/pause.svg"
     this.imgCancion = this.dataSongs[0].img
     this.artista = this.dataSongs[0].artista
     this.nombreCancion = this.dataSongs[0].nombre
