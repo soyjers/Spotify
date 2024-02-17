e05eb8a65c884f086e5cd229a990b8e412edb710
unknown <gustavogaviria_24@hotmail.com>
2024-02-16 23:52:54 -0500

    merge con thomas

--- Spotify/src/app/app.config.ts
+++ Spotify/src/app/app.config.ts
@@ -4,12 +4,6 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 
-<<<<<<< HEAD
-
-
-
-=======
->>>>>>> Gustavo
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
