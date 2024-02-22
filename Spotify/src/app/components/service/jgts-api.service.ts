b93797462aa2038d62e1a2b850556039b377eedd
unknown <gustavogaviria_24@hotmail.com>
2024-02-22 18:31:53 -0500

    avance

--- Spotify/src/app/components/service/jgts-api.service.ts
+++ Spotify/src/app/components/service/jgts-api.service.ts
@@ -42,38 +42,19 @@ export class JgtsAPIService {
 
 
 
-
-
-
-
-
-
     /* Albumes */
-    postAlbumes(albumId: string) {
-        return this.http.get(`${this.urlApi}/create-album/${albumId}`)
-    }
-
-
-
-
-
-
-    // estaLogueado() : boolean{
-    //     let estado = (sessionStorage.getItem('token')) ? true : false
-    //     return estado
-    // }
     getAlbumes() {
         const headers = new HttpHeaders().set('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1YzZiYWIyZjBiMDgwMzQ3ZWJkODIzOSIsInJvbCI6ImFkbWluIiwiaWF0IjoxNzA3NTI0OTAzLCJleHAiOjE3MDc1Mjg1MDN9.n7XqQr9aFmm_mOu-cKROiwxj0zjYJYlUR5Moq0VqJ_o')
         return this.http.get(`${this.urlApi}/find-albums`, { headers })
     }
 
-    getAlbum(idAlbum: string|null) {
+    getAlbum(idAlbum: string | null) {
         return this.http.get(`${this.urlApi}/find-album/${idAlbum}`)
     }
 
     postAlbum(dataAlbum: any) {
         return this.http.post(`${this.urlApi}/create-album`, dataAlbum);
-      }
+    }
 
     deleteaAlbum(idAlbum: string) {
         return this.http.delete(`${this.urlApi}/eliminar-album/${idAlbum}`)
