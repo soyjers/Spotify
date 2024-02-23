60483cce984501dfa08be401c5b37b64fbe7691c
soyjers <borkgoncoc@gmail.com>
2024-02-23 18:43:11 -0500

    la cague

---
+++ Spotify/src/app/components/artistas/artistas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-artistas',
+  standalone: true,
+  imports: [],
+  templateUrl: './artistas.component.html',
+  styleUrl: './artistas.component.css'
+})
+export class ArtistasComponent {
+
+}
