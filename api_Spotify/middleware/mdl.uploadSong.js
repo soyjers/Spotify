39a7dea787954e141ae179b8e96de4b8b78255d3
soyjers <borkgoncoc@gmail.com>
2024-02-29 18:51:50 -0500

    FIX mdl multer

--- api_Spotify/middleware/mdl.uploadSong.js
+++ api_Spotify/middleware/mdl.uploadSong.js
@@ -16,7 +16,7 @@ const storage = multer.diskStorage({
     },
 
     filename: function (req, file, cb) {
-        cb(null, `${Date.now()}_${file.originalname}`);
+        cb(null, `${Date.now()}`);
     }
 });
 
