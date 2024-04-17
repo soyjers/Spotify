167500376c7c2ab6f3e7b602fd7a060ca5a6023d
Gustavo <gustavogaviria_24@hotmail.com>
2024-04-16 20:24:22 -0500

    clear-frontend

--- Spotify/src/app/guards/autenticacion.guard.ts
+++
@@ -1,8 +0,0 @@
-import { inject } from '@angular/core';
-import { CanMatchFn } from '@angular/router';
-import { SpotifyAPIService } from '../components/service/spotify-api.service';
-
-export const autenticacionGuard: CanMatchFn = (route, segments) => {
-  const SpotifyService = inject(SpotifyAPIService)
-return SpotifyService.estaLogueado()
-};
